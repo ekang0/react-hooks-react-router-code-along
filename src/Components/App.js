3c8be9206d21f747df30bd83b1da5b2d5fded3b2
Esther Kang <eskang87@gmail.com>
2022-09-20 22:12:20 -0700

    commit tree f8b39c6c3dd931f28227fc3de092dbb7db220a46

---
+++ src/Components/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar"; 
+import { Route, Switch } from "react-router-dom"
+
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <Switch> 
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+};
+
+export default App;
